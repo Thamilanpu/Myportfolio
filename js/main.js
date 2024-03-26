8162413a4767634961f6c1c94fab442b54ab2f2b
Thamilanpu <sthamil1016@gmail.com>
2024-03-26 12:02:53 +0530

    portfolio updated

--- js/main.js
+++ js/main.js
@@ -84,3 +84,34 @@ $(function() {
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /**
+   * Intro type effect
+   */
+  const typed = select('#typed-text')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
